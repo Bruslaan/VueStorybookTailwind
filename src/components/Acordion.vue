b54b6c9bc94b79be8a7a8981f9f34ec06ef5c7f3
bruslaan <bruslan.no@gmail.com>
2020-03-17 15:49:06 +0100

    firebase hosting added

--- src/components/Acordion.vue
+++ src/components/Acordion.vue
@@ -6,14 +6,16 @@
     >
       <div>
         <h4 class="text-gray-800 font-light">{{name}}</h4>
-        <ul v-if="open" class="mt-3">
-          <li class="pl-3 py-1 hover:text-gray-800 text-gray-600">
-            <a href>First Link</a>
-          </li>
-          <li class="pl-3 py-1 hover:text-gray-800 text-gray-600">
-            <a href>Second Link</a>
-          </li>
-        </ul>
+        <transition>
+          <ul v-if="open" class="mt-3">
+            <li class="pl-3 py-1 hover:text-gray-800 text-gray-600">
+              <a href>First Link</a>
+            </li>
+            <li class="pl-3 py-1 hover:text-gray-800 text-gray-600">
+              <a href>Second Link</a>
+            </li>
+          </ul>
+        </transition>
       </div>
       <div>
         <h4>{{state}}</h4>
@@ -25,10 +27,10 @@
 <script>
 export default {
   props: {
-      name: {
-          type:String,
-          default: "mother"
-      }
+    name: {
+      type: String,
+      default: "mother"
+    }
   },
   computed: {
     state() {
