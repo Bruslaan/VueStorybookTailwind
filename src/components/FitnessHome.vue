9719be8a50cd53d0d88039a9bf81a4505ddead13
bruslaan <bruslan.no@gmail.com>
2020-03-23 20:05:00 +0100

    ka

--- src/components/FitnessHome.vue
+++ src/components/FitnessHome.vue
@@ -2,127 +2,58 @@
   <div class="md:mt-20">
     <div class="flex justify-center items-center h-24 sticky top-0 bg-white">
       <h4 class="text-gray-300 text-lg mr-4">Woche 1</h4>
-      <h1 class="text-4xl text-gray-900 font-bold">Woche 2</h1>
+      <h1 class="text-3xl text-gray-900 font-bold">Woche 2</h1>
       <h4 class="text-gray-300 text-lg ml-4">Woche 3</h4>
     </div>
 
     <div class="w-full">
-      <div class="px-12 my-12 max-w-lg mx-auto">
-        <div class="flex justify-between items-center">
+      <div class="px-6 my-6 max-w-lg mx-auto">
+        <div class="relative flex justify-between items-center border-green-300 border px-8 rounded py-4">
           <div>
-            <h1 class="text-xl">Tag 1</h1>
+            <h1 class="text-xl text-gray-700">Tag 1</h1>
             <span class="text-gray-500 text-sm">01.02.2020</span>
           </div>
-          <div>
+          <div class="bg-white rounded-full border-green-300 border p-4">
             <img src="../assets/zondicons/checkmark.svg" class="w-5 text-center h-5" />
-          </div>
-        </div>
-      </div>
-      <div class="px-12 my-12 max-w-lg mx-auto">
-        <div class="flex justify-between items-center">
-          <div>
-            <h1 class="text-xl">Tag 2</h1>
-            <span class="text-gray-500 text-sm">03.02.2020</span>
-          </div>
-          <div>
-            <img src="../assets/zondicons/checkmark.svg" class="w-5 text-center h-5" />
-          </div>
-        </div>
-      </div>
-      <div class="px-12 my-12 max-w-lg mx-auto">
-        <div class="flex justify-between items-center">
-          <div>
-            <h1 class="text-xl">Tag 3</h1>
-            <span class="text-gray-500 text-sm">05.02.2020</span>
-          </div>
-          <div>
-            <a href="#">Start</a>
-          </div>
-        </div>
-      </div>
-      <div class="px-12 my-12 max-w-lg mx-auto">
-        <div class="flex justify-between items-center">
-          <div>
-            <h1 class="text-xl">Tag 3</h1>
-            <span class="text-gray-500 text-sm">05.02.2020</span>
-          </div>
-          <div>
-            <a href="#">Start</a>
-          </div>
-        </div>
-      </div>
-      <div class="px-12 my-12 max-w-lg mx-auto">
-        <div class="flex justify-between items-center">
-          <div>
-            <h1 class="text-xl">Tag 3</h1>
-            <span class="text-gray-500 text-sm">05.02.2020</span>
-          </div>
-          <div>
-            <a href="#">Start</a>
-          </div>
-        </div>
-      </div>
-      <div class="px-12 my-12 max-w-lg mx-auto">
-        <div class="flex justify-between items-center">
-          <div>
-            <h1 class="text-xl">Tag 3</h1>
-            <span class="text-gray-500 text-sm">05.02.2020</span>
-          </div>
-          <div>
-            <a href="#">Start</a>
-          </div>
-        </div>
-      </div>
-      <div class="px-12 my-12 max-w-lg mx-auto">
-        <div class="flex justify-between items-center">
-          <div>
-            <h1 class="text-xl">Tag 3</h1>
-            <span class="text-gray-500 text-sm">05.02.2020</span>
-          </div>
-          <div>
-            <a href="#">Start</a>
-          </div>
-        </div>
-      </div>
-      <div class="px-12 my-12 max-w-lg mx-auto">
-        <div class="flex justify-between items-center">
-          <div>
-            <h1 class="text-xl">Tag 3</h1>
-            <span class="text-gray-500 text-sm">05.02.2020</span>
-          </div>
-          <div>
-            <a href="#">Start</a>
-          </div>
-        </div>
-      </div>
-      <div class="px-12 my-12 max-w-lg mx-auto">
-        <div class="flex justify-between items-center">
-          <div>
-            <h1 class="text-xl">Tag 33</h1>
-            <span class="text-gray-500 text-sm">05.02.2020</span>
-          </div>
-          <div>
-            <a href="#">Start</a>
           </div>
         </div>
       </div>
     </div>
-    <!-- 
-    <div class="mt-5 sm:mt-8 sm:flex sm:justify-center px-6">
-      <div class>
-        <a
-          href="#"
-          class="shadow w-full flex items-center justify-center px-8 py-3 border border-transparent text-base leading-6 font-medium rounded-full text-white bg-gray-600 hover:bg-indigo-500 focus:outline-none focus:shadow-outline transition duration-150 ease-in-out md:py-3 md:text-lg md:px-20"
-        >Get started</a>
-      </div>
-    </div>-->
 
-    <!-- bottom NAv -->
+
+
+    <div class="w-full">
+      <div class="px-6 my-6 max-w-lg mx-auto">
+        <div class="relative flex justify-between items-center border-green-300 border px-8 rounded py-4">
+          <div>
+            <h1 class="text-xl text-gray-700">Tag 2</h1>
+            <span class="text-gray-500 text-sm">04.02.2020</span>
+          </div>
+          <div class="bg-white rounded-full border-green-300 border p-4">
+            <img src="../assets/zondicons/checkmark.svg" class="w-5 text-center h-5" />
+          </div>
+        </div>
+      </div>
+    </div>
+
+
+    <div class="w-full">
+      <div class="px-6 my-6 max-w-lg mx-auto">
+        <div class="flex justify-between items-center border px-8 rounded py-4">
+          <div>
+            <h1 class="text-xl">Tag 3</h1>
+            <span class="text-gray-500 text-sm">Heute</span>
+          </div>
+          
+        </div>
+      </div>
+    </div>
+
     <div
-      class="nav flex fixed bottom-0 inset-x-0 bg-white fixed border-t  w-full md:hidden justify-evenly"
+      class="nav flex fixed bottom-0 inset-x-0 bg-white fixed border-t w-full md:hidden justify-evenly"
     >
       <a class>
-        <img src="../assets/zondicons/home.svg" class="w-5 text-center h-5" />
+        <img src="../assets/zondicons/hot.svg" class="w-5 text-center h-5" />
       </a>
       <a class>
         <img src="../assets/zondicons/checkmark.svg" class="w-5 text-center h-5" />
@@ -139,12 +70,9 @@ export default {};
 </script>
 
 <style>
-.nav { 
-  background: white; 
-  padding:  
-    10px
-    env(safe-area-inset-right, 20px)
-    max(10px, env(safe-area-inset-bottom, 20px))
-    env(safe-area-inset-left, 20px);
+.nav {
+  background: white;
+  padding: 10px env(safe-area-inset-right, 20px)
+    max(10px, env(safe-area-inset-bottom, 20px)) env(safe-area-inset-left, 20px);
 }
 </style>
